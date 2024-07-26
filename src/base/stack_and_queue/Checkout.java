d55bd81db9bf8c09493910a6ac2ae9a2ac4535be
hedeoer <2138@qq.com>
2024-07-26 19:25:49 +0800

    设计自助结算系统-双端队列-单调不增队列实现

--- src/base/stack_and_queue/Checkout.java
+++ src/base/stack_and_queue/Checkout.java
@@ -2,6 +2,7 @@ package base.stack_and_queue;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 请设计一个自助结账系统，该系统需要通过一个队列来模拟顾客通过购物车的结算过程，需要实现的功能有：
@@ -23,36 +24,43 @@ public class Checkout {
     * 2. 移除结算商品同时是否需要考虑购物车的最高商品最高价格，考虑，如何维护最值？
     * */
     // 使用链表实现队列
-    Deque<Integer> deque ;
-    Deque<Integer> maxDeque;
+    // 购物车商品价格队列
+    Queue<Integer> queue;
+    // "单调不增队列"
+    Deque<Integer> maxQueue;
     public Checkout() {
         // 初始化购物车商品
-        deque  = new LinkedList<Integer>();
-        maxDeque  = new LinkedList<Integer>();
+        queue = new LinkedList<Integer>();
+        maxQueue = new LinkedList<Integer>();
     }
 
     public int get_max() {
-        // peekLast是获取队列首元素吗？
-        return maxDeque.isEmpty() ? -1 : maxDeque.peekFirst();
+        // 单调不增队列不为空，队首即为最大值
+        return maxQueue.isEmpty() ? -1 : maxQueue.peekFirst();
     }
 
     public void add(int value) {
-        // 维护单调队列maxDeque,保证maxDeque的队首总是购物车中的商品最大值
-        while (!maxDeque.isEmpty() && maxDeque.peekLast() < value) {
-            maxDeque.removeLast();
+        // 维护单调不增队列maxDeque,保证maxDeque的队首总是购物车中的商品最大值
+        while (!maxQueue.isEmpty() && maxQueue.peekLast() < value) {
+            maxQueue.removeLast();
 
         }
-        maxDeque.addLast(value);
-        deque.add(value);
+        // 加入结算商品
+        queue.add(value);
+        // 添加到单调不增队列
+        maxQueue.add(value);
     }
 
     public int remove() {
-        if(deque.isEmpty()) return -1;
+        // 计算商品为空，则返回-1
+        if(queue.isEmpty()) return -1;
 
-        if(deque.peekFirst().equals(maxDeque.peekFirst())){
-            maxDeque.removeFirst();
+        //如果结算商品和单调不增队列的队首相等，则移除单调不增队列的队首
+        if(queue.peek().equals(maxQueue.peekFirst())){
+            maxQueue.removeFirst();
         }
-        return deque.peek();
+        // 移除结算商品
+        return queue.remove();
     }
 
     public static void main(String[] args) {
